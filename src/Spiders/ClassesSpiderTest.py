7fc208edbd9a8533042994404e2548fbcb26d661
Johnzeng <Johnz@misfit.com>
2016-08-04 14:36:17 +0800

    1.6.0 version is on

--- src/Spiders/ClassesSpiderTest.py
+++ src/Spiders/ClassesSpiderTest.py
@@ -15,8 +15,7 @@ logger = logging.getLogger('Tester')
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)  
   
-logger.info('first info message')  
-logger.debug('first debug message') 
+
 class PartsTester:
     def brufPull(self, url):
       #This is a tester, so should get error if any wrong is happen
@@ -26,19 +25,24 @@ class PartsTester:
       return msg
 
     def RunTest(self, parts, version, url, testClassesList):
+        logger.debug("now begin to do test for url:" + url)
         try:
             root = self.brufPull(url)
             versions = ClassesSpider.SpiderParts.versionReg.findall(root)
             #check version
             for v in versions:
                 if -1 == v.find(version):
+                    logger.error("can not find version")
                     return False
             #check package summary page 
             packagePage = self.brufPull(url + parts.PackageSummaryPage)
             #check package summary page re
             packages = parts.PackageRefRe.findall(packagePage)
             if len(packages) == 0:
+                logger.error("can not find packages")
                 return False
+            for p in packages:
+                logger.debug(p)
             #check classes page
             classesListPage = self.brufPull(url + parts.ClassesListPage)
             #check the classes re
@@ -47,6 +51,10 @@ class PartsTester:
 
             methodFlag = False
             nestedClassFlag = True
+            if len(classes) == 0:
+                print classesListPage
+                logger.error("can not get classes")
+                return False
             for c in classes:
                 logger.debug("get page:" + c[0])
                 if c[0].replace(".html","").replace("/",".") in tcSet:
@@ -55,7 +63,7 @@ class PartsTester:
                     classPage = self.brufPull(url + c[0])
                     for s in parts.SummaryRe.findall(classPage):
                         for ss in parts.MemberSummaryRe.findall(s):
-                            if ss[0].find("nested") != -1:
+                            if ss[0].lower().find("nested") != -1:
                                 nestedClassFlag = False
                                 for nestedC in parts.SubClassRe.findall(ss[1]) :
                                     logger.debug("get nestedClass:" + nestedC[0])
@@ -71,17 +79,15 @@ class PartsTester:
             return False
 
 
-class TestStringMethods(unittest.TestCase):
-#    def test_whole(self):
-#      rootUrl = 'http://api.mongodb.com/java/current/'
-#      spider = ClassesSpider.Spider(rootUrl, "JavaMongo")
-#      try:
-#          spider.run()
-#        except:
-#            tb = traceback.format_exc()
-#            print(tb)
+class TestBeta(unittest.TestCase):
+    def test_BetaParts(self,):
+      parts = ClassesSpider.SpiderPartsBeta
+      url = 'http://api.mongodb.com/java/2.0/'
+      tester = PartsTester()
+      ret = tester.RunTest(parts, "1.6.0", url, ['org.bson.BasicBSONCallback', 'com.mongodb.DB.WriteConcern', 'com.mongodb.MongoException'])
+      self.assertTrue(ret)
 
-
+class TestAlpha(unittest.TestCase):
     def test_AlphaParts(self,):
       parts = ClassesSpider.SpiderPartsAlpha
       url = 'http://api.mongodb.com/java/current/'
@@ -89,6 +95,13 @@ class TestStringMethods(unittest.TestCase):
       ret = tester.RunTest(parts, "1.8.0", url, ['org.bson.AbstractBsonReader', 'com.mongodb.WriteConcern'])
       self.assertTrue(ret)
 
+
 if __name__ == '__main__':
-    unittest.main()
+    alphaSuite = unittest.TestLoader().loadTestsFromTestCase(TestAlpha)
+    betaSuite = unittest.TestLoader().loadTestsFromTestCase(TestBeta)
+    alltests = unittest.TestSuite([
+        alphaSuite,
+        betaSuite
+        ])
+    unittest.TextTestRunner(verbosity=2).run(alltests)
 
